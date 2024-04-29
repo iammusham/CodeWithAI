a8506ac27306c72163cf673e03c0a012971289c5
Musham khan <135441709+iammusham@users.noreply.github.com>
2024-04-29 11:32:53 +0530

    update OpenAI API Tutorial/telegram_bot_gpt_3.5_turbo.py

---
+++ OpenAI API Tutorial/telegram_bot_gpt_3.5_turbo.py
@@ -0,0 +1,31 @@
+from openai import OpenAI 
+
+model = "gpt-3.5-turbo"
+client = OpenAI(api_key="openai_api_key")
+
+def askai(user_input):
+  response = client.chat.completions.create(model=model,messages=[{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": user_input}])  
+  output = response.choices[0].message.content  
+  return output
+
+import loggingfrom telegram 
+import Updatefrom telegram.ext 
+import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):  
+  await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):  
+  await context.bot.send_message(chat_id=update.effective_chat.id, text=askai(update.message.text))
+
+if __name__ == '__main__':  
+  application = ApplicationBuilder().token('7181684065:AAHnVl1ZcWBuwPTj8UXNydkR3lbfbv8mz0I').build()      
+  
+  start_handler = CommandHandler('start', start)  
+  echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)      
+  application.add_handler(start_handler)  
+  application.add_handler(echo_handler)
+
+  application.run_polling()
