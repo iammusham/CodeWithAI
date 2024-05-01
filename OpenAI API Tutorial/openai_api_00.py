3b654854029a25bd44f2ed706d42d469a0084410
Musham khan <135441709+iammusham@users.noreply.github.com>
2024-05-01 15:44:58 +0530

    update OpenAI API Tutorial/openai_api_00.py

---
+++ OpenAI API Tutorial/openai_api_00.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+openai_api_key = "your_api_key"
+
+model = "gpt-3.5-turbo"
+client = OpenAI(api_key=openai_api_key)
+
+while True:
+  user_input = input("Ask me anything: ")
+  messages = [{"role":"system","content":"Act as a Jarvis like assistant"},{"role":"user","content":user_input}]
+  response = client.chat.completions.create(model=model,messages=messages,max_tokens=50)
+  output = response.choices[0].message.content
+  tokens = response.usage.total_tokens
+  print(f"ChatGPT: {output}")
+  print()
+  print(f"Tokens used {tokens}")
+  print()
