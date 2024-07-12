1de7c510f86c7b8009377c792f76f13a53354255
Musham khan <135441709+iammusham@users.noreply.github.com>
2024-07-12 21:02:24 +0530

    update OpenAI API Tutorial/chatgpt_create_chatgpt.py

--- OpenAI API Tutorial/chatgpt_create_chatgpt.py
+++ OpenAI API Tutorial/chatgpt_create_chatgpt.py
@@ -2,6 +2,7 @@ from openai import OpenAI
 import time, threading
 from keys import openai_api_key
 
+model="gpt-3.5-turbo"
 client = OpenAI(api_key=openai_api_key)
 
 while True:
@@ -9,7 +10,8 @@ while True:
   prompt = input("You: ")
   if prompt=="q":
     break
-  response = client.chat.completions.create(model="gpt-3.5-turbo",messages=[{"role": "system", "content": "Act as ChatGPT"},{"role": "user", "content": print}],max_tokens=40)
+  messages = [{"role": "system", "content": "You are Alpha, Musham 's Personal assistant"},{"role": "user", "content": prompt}]
+  response = client.chat.completions.create(model=model,messages= messages,max_tokens=30)
   print()
   output=response.choices[0].message.content
   print(f"ChatGPT: {output}")
