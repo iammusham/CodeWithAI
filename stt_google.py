62d0d9550551e2382540ed296c2e5b5fe15c30d6
iammusham <mushamkhan1321198113@gmail.com>
2024-07-26 19:30:28 +0530

    stt google

---
+++ stt_google.py
@@ -0,0 +1,24 @@
+"""
+Speech to text using Python SpeechRecognition with Google
+"""
+
+"""
+Requirements:
+Python: (winget install python) for windows only
+SpeechRecognition Lib: (pip install SpeechRecognition)
+"""
+import speech_recognition as sr
+
+# Audio File Path
+audio_file = "C:\\Users\\Acer\\Downloads\\CodeWithAI\\sample_audio.wav"
+
+r = sr.Recognizer()
+with sr.AudioFile(audio_file) as source:
+    audio = r.record(source)
+try:
+    transcript = r.recognize_google(audio)
+except sr.UnknownValueError:
+    transcript = "audio error"
+except sr.RequestError as e:
+    transcript = "Request error"
+print(transcript)
